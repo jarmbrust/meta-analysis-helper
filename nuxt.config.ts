d71a6a408a6cc86c02f6a017a76c353d94366ca6
James Armbrust <james@Jamess-MBP.local>
2023-11-30 09:57:49 -0700

    installing packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@pinia/nuxt',
+    ['@nuxtjs/eslint-module', { /* module options */ }],
+    '@nuxtjs/tailwindcss'
+  ],
+  devtools: { enabled: true },
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  },
+  
 })
